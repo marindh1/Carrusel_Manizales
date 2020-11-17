5694821d24c98e8dcf474ccb916672e996d6085c
Sebastian <sbitashenaol@gmail.com>
2020-11-16 21:04:27 -0500

    subida de pqr

---
+++ MS_PQR/Models/MS_PQRContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MS_PQR.Models
+{
+    public partial class MS_PQRContext : DbContext
+    {
+        public MS_PQRContext()
+        {
+        }
+
+        public MS_PQRContext(DbContextOptions<MS_PQRContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pqr> Pqr { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress;Initial Catalog=MS_PQR;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pqr>(entity =>
+            {
+                entity.ToTable("PQR");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Descripcion)
+                    .IsRequired()
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Timestamp)
+                    .IsRequired()
+                    .HasColumnName("timestamp")
+                    .IsRowVersion()
+                    .IsConcurrencyToken();
+
+                entity.Property(e => e.Tipo)
+                    .IsRequired()
+                    .HasColumnName("tipo")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UsuarioId).HasColumnName("usuario_id");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MS_PQR/Models/Pqr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MS_PQR.Models
+{
+    public partial class Pqr
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo { get; set; }
+        public int UsuarioId { get; set; }
+        public byte[] Timestamp { get; set; }
+    }
+}
